7072e9be23f5a49a35610f9e6aedd52b71b9eb4f
alikhantareen <creditableboy@gmail.com>
2021-09-28 03:26:35 +0500

    update v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import ItemScreen from './components/ItemScreen'
+import HomeScreen from "./components/HomeScreen";
+import CartScreen from "./components/CartScreen";
+import Checkout from "./components/Checkout";
+import {Switch, Route} from 'react-router-dom'
+import CartItems from "./components/CartItems";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Switch>
+            <Route path="/" exact={true}>
+              <HomeScreen />
+            </Route>
+            <Route path="/items/:id" exact={true}>
+              <ItemScreen />
+            </Route>
+            <Route path="/items/:id/cart">
+              <CartScreen />
+            </Route>
+          <Route path="/checkout">
+              <Checkout />
+          </Route>
+          <Route to="/mycart">
+            <CartItems />
+          </Route>
+        </Switch>
     </div>
   );
 }
